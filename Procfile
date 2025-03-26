026772043d2f92bc919f4056f0de6576745abd03
hamed <hamed.azizi.xear@gmail.com>
2025-03-26 02:04:28 -0400

    delpoy

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn cms.wsgi --log-file -
+worker : celery -A cms worker --loglevel=info
--- cms/settings.py
+++ cms/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from pathlib import Path
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,9 +88,6 @@ DATABASES = {
     }
 }
 
-# DATABASES["default"] = dj_database_url.parse('postgresql://communication_django_render_user:DY7zOEhpnrcSvN2K63ivfbupQiXH7OrQ@dpg-cutei9tds78s738v5sv0-a.oregon-postgres.render.com/communication_django_render')
-
-# postgresql://communication_django_render_user:DY7zOEhpnrcSvN2K63ivfbupQiXH7OrQ@dpg-cutei9tds78s738v5sv0-a.oregon-postgres.render.com/communication_django_render
 
 
 # Password validation
@@ -126,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR , 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
